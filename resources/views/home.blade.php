2e42d939c2a4f71f3cdfaba62d72e45f2dafd6e9
thialms <thiiago.allmeida44@gmail.com>
2025-05-27 02:55:29 -0300

    feat: add a menu page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,13 +10,17 @@
     {{-- Redireciona para login --}}
 @endif
 
-<div class="flex flex-col items-center justify-center min-h-screen py-6 sm:py-12 lg:py-24 bg-white">
+<div class="flex flex-col items-center justify-center min-h-screen py-6 sm:py-12 lg:py-24 bg-white gap-6">
 
     <div class="mb-8 text-center font-semibold cursor-default">
         <p class="text-4xl font-jost font-bold leading-none">Mercado do</p>
         <p class="text-5xl font-jost font-bold text-blue-800 -mt-2">Fernando</p>
     </div>
 
-    
+    <button class="w-100 py-12 text-3xl font-bold font-jost bg-blue-200 rounded-3xl shadow-md mb-2 cursor-pointer">Efetuar venda</button>
+    <button class="w-100 py-12 text-3xl font-bold font-jost bg-blue-400 rounded-3xl shadow-md mb-2 cursor-pointer">Relatório de vendas</button>
+    <button class="w-100 py-12 text-3xl font-bold font-jost bg-blue-600 text-white rounded-3xl shadow-md mb-2 cursor-pointer">Lorem</button>
+    <button class="w-100 py-12 text-3xl font-bold font-jost bg-blue-800 text-white rounded-3xl shadow-md cursor-pointer">Lorem</button>
+</div>
 
 @endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mercado do Fernando</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     <div>
         @yield('content')
     </div>
-    
 </body>
 </html>
