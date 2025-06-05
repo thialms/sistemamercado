5b24e68b25b314642fadae9cfe8ddf330aee3bae
thialms <thiiago.allmeida44@gmail.com>
2025-06-04 22:50:56 -0300

    build: testes

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mercado do Fernando</title>
     {{-- Adicionar um favicon de mercado --}}
-    <script src="https://unpkg.com/@ericblade/quagga2/dist/quagga.min.js"></script>
+    @vite('resources/css/app.css')
 </head>
 <body>
     <div>
