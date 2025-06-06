c39dfa527ae0a7beafba936555816ebaf5273ded
gabe-pud <gabrielgarciasantana3@gmail.com>
2025-06-06 20:01:27 -0300

    feat: add crete_produtos_table migration

---
+++ database/migrations/2025_06_06_220811_create_produtos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id(); // codigo de barras
+            $table->string('nome')->unique();
+            $table->string('marca');
+            $table->double('prco_venda');
+            $table->double('custo');
+            $table->integer('estoque_atual');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
