c7fa1bbd1834fa48e3d854cbaf6bf54c05fa2cd7
levidowski <leviferreira1677@gmail.com>
2025-06-06 21:45:31 -0300

    feat: add create_custos_historicos_table migration

---
+++ database/migrations/2025_06_07_003052_create_custos_historicos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('custos_historicos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('produto_id');
+            $table->double('custo');
+            $table->unsignedBigInteger('origem_movimento_id');
+            $table->string('observacao');
+            $table->timestamps();
+
+            $table->foreign('produto_id')->references('id')->on('produtos');
+            $table->foreign('origem_movimento_id')->references('id')->on('estoque_movimentos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('custos_historicos');
+    }
+};
