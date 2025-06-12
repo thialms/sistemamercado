40eacb2e278e9a02898a5cede2d27d312bdefe18
levidowski <leviferreira1677@gmail.com>
2025-06-11 23:55:48 -0300

    feat: add charts for daily and monthly earnings in home view

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,5 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
-    server: {
-        host: '0.0.0.0',
-        hmr: {
-            host: 'https://e51d-2804-868-d041-30c4-f4e4-d6f9-6959-1bbf.ngrok-free.app/',
-        },
-    },
+    
 });
